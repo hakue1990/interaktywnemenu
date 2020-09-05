b130967d3647b8c6d97d7cdf4f45d297e078db82
Adam Hałdaś <adam.haldas@gmail.com>
2020-09-05 22:37:04 +0200

    Updates

---
+++ precache-manifest.68824d77493fab285bc8c0f5d3b7509f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d7dc79eb18de3078b7e20346050fb78",
+    "url": "/interakywnemenu/index.html"
+  },
+  {
+    "revision": "948e254e2571bfd35c34",
+    "url": "/interakywnemenu/static/css/main.365d6d50.chunk.css"
+  },
+  {
+    "revision": "c7551cde6a3fd575842f",
+    "url": "/interakywnemenu/static/js/2.3f049e68.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/interakywnemenu/static/js/2.3f049e68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "948e254e2571bfd35c34",
+    "url": "/interakywnemenu/static/js/main.f9355f83.chunk.js"
+  },
+  {
+    "revision": "d178b4d63e00d9fc107e",
+    "url": "/interakywnemenu/static/js/runtime-main.944f7189.js"
+  }
+]);
