eb99e1802a8dd04ca54da3fd34a4dfe30f8b51a5
Adam Hałdaś <adam.haldas@gmail.com>
2020-09-06 12:45:18 +0200

    add rwd css

--- src/components/App.js
+++ src/components/App.js
@@ -8,14 +8,14 @@ class App extends React.Component {
     items: [
       { id: 1, name: "ROYAL GORILLA", active: false },
       { id: 2, name: "NORTHERN LIGHT", active: false },
-      { id: 3, name: "AMNESIA HAZE", active: true },
-      { id: 4, name: "GREEN GELATO", active: true },
+      { id: 3, name: "AMNESIA HAZE", active: false },
+      { id: 4, name: "GREEN GELATO", active: false },
       { id: 5, name: "SWEET ZZ", active: false },
-      { id: 6, name: "WHITE WIDOW", active: true },
-      { id: 7, name: "SKUNK XL", active: true },
-      { id: 8, name: "FAT BANANA", active: true },
-      { id: 9, name: "OG KUSH", active: true },
-      { id: 10, name: "SOUR DIESEL", active: true },
+      { id: 6, name: "WHITE WIDOW", active: false },
+      { id: 7, name: "SKUNK XL", active: false },
+      { id: 8, name: "FAT BANANA", active: false },
+      { id: 9, name: "OG KUSH", active: false },
+      { id: 10, name: "SOUR DIESEL", active: false },
     ],
   };
   handleClick = (id) => {
@@ -31,13 +31,11 @@ class App extends React.Component {
   };
   render() {
     return (
-      <React.Fragment>
+      <div className="container">
         <Header items={this.state.items} />
         <ListItems items={this.state.items} click={this.handleClick} />
-        <br />
-        <br />
         <h4>Created by Ah</h4>
-      </React.Fragment>
+      </div>
     );
   }
 }
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Item = (props) => {
-  return <li onClick={() => props.click(props.id)}>{props.item.name}</li>;
+  return (
+    <li
+      style={props.active ? { fontWeight: "bold", color: "white" } : {}}
+      onClick={() => props.click(props.id)}>
+      {props.item.name}
+    </li>
+  );
 };
 
 export default Item;
